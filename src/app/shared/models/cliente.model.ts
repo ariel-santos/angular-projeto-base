e5a7bb9e90e05cb0822bdce512f2e2cdd243e6c7
ArielSantos <arielsantos.ac@gmail.com>
2021-07-04 11:18:55 -0300

    feat: create lista cliente

---
+++ src/app/shared/models/cliente.model.ts
@@ -0,0 +1,9 @@
+export class Cliente {
+  id_cliente: string;
+  codigo: string;
+  cliente: string;
+  nome: string;
+  cpf: string;
+  sexo: string;
+  email: string;
+}
--- src/app/views/cliente/cliente-lista/cliente-lista.component.ts
+++ src/app/views/cliente/cliente-lista/cliente-lista.component.ts
@@ -1,4 +1,8 @@
+import { Cliente } from './../../../shared/models/cliente.model';
+import { ClienteService } from './../../../services/cliente.service';
 import { Component, OnInit } from '@angular/core';
+
+declare var M: any;
 
 @Component({
   selector: 'app-cliente-lista',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cliente-lista.component.css']
 })
 export class ClienteListaComponent implements OnInit {
+  clienteList: Cliente[];
 
-  constructor() { }
+  constructor(
+    private clienteService: ClienteService
+  ) { }
 
   ngOnInit(): void {
+    this.getClientes();
+  }
+
+  getClientes() {
+    this.clienteService.getClientes().subscribe(
+      (res: Cliente[]) => {
+        console.log(res);
+        this.clienteList = res;
+      },
+      err => {
+
+      }
+    )
+  }
+
+  editCliente() {
+    console.log('editCliente()');
+  }
+
+  openModal() {
+    const elem = document.getElementById('modal-confirm-delete');
+    M.Modal.init(elem);
+    const instance = M.Modal.getInstance(elem);
+    instance.open();
+  }
+
+  deleteCliente() {
+    // this.clienteService.deleteCLiente().subscribe(
+    //   (res: any) => {
+    //     console.log(res);
+    //   }
+    // )
   }
 
 }
